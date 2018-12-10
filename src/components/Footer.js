75c3e691ac67e46439b282d84a05af7d4e73a189
lukaszf10 <43759602+lukaszf10@users.noreply.github.com>
2018-12-10 22:03:44 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,38 @@ import {colors} from '../constants/colors'
 
 const Footer = () => 
 <div>
-<div className="footer"/>
+<div className="footer">
+	<div className="Table">
+      <tr>
+          <td>Address</td>
+		  <td align="right"><b>STEAM4POOR APP</b></td>
+      </tr>
+	  <tr>
+          <td>132 Golden St.  San Francisco,  USA</td>
+		  <td align="right">Copyright SGGW &#169; 2018</td>
+      </tr>
+	</div>
+
 <style jsx>{`
 .footer {
     background: ${colors.footer.bg};
     color: ${colors.footer.font};
     width: 100%;
-    height: 10em;
+    height: 4em;
     bottom: 0;
     position: absolute;
 }
+.Table{
+	margin: 7px 10px 10px 30px;
+}
+td {
+	color:white;
+	width: 980px;
+    height: 13px;
+}
+
 `}</style>
+</div>
 </div>
 
 export default Footer
