602a1fd47d4b39cb6d6eff80e53ab328705ed170
Maria Zagrodzka <maria.zagrodzka1@gmail.com>
2019-01-30 14:54:42 +0100

    Zmiana tła na stronach logowania rejestracji itp.

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import SignComponent from '../components/SignComponent' 
+import Background from '../images/obrazek_strona_glowna'
+import sygnet from '../images/sygnet.svg' 
 
 const ResetPassword = () => 
-<div className="reset-password">
+<div className="reset-password" style={{backgroundImage: `url(${Background})`}}>
+<div className="information-wrapper">
+<img className="sygnet" src={sygnet}></img>
+<span className="information">Pierwsza platforma dla biednych ludzi</span>
+</div>
 <div className="reset-password_change">
 <SignComponent />
 </div>
 <style jsx>{`
+.information-wrapper {
+    height: 100%;
+    display:grid;
+    grid-auto-flow: column;
+    place-content: center;
+}
+.sygnet {
+    margin: auto 40px;
+    width: 140px;
+    height:140px;
+}
+.information {
+    color: white;
+    font-size: 60px;
+    width: 550px;
+    font-family: Lato;
+    font-weight:bold;
+    letter-spacing: 1px;
+}
 .reset-password {
     background: #1a1d42;
     width: 100vw;
     height: 100%;
     display:grid;
-    place-content: center end;
+    grid-template-columns: 60% 40%;
+    place-content: center;
 }
 .reset-password_change {
     width: 600px;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import SignComponent from '../components/SignComponent'
+import Background from '../images/obrazek_strona_glowna'
+import sygnet from '../images/sygnet.svg' 
 
 const SignIn = () => 
-<div className="sign-in">
+<div className="sign-in" style={{backgroundImage: `url(${Background})`}}>
+<div className="information-wrapper">
+<img className="sygnet" src={sygnet}></img>
+<span className="information">Pierwsza platforma dla biednych ludzi</span>
+</div>
 <div className="comp"><SignComponent /></div>
 <style jsx>{`
+.information-wrapper {
+    height: 100%;
+    display:grid;
+    grid-auto-flow: column;
+    place-content: center;
+}
+.sygnet {
+    margin: auto 40px;
+    width: 140px;
+    height:140px;
+}
+.information {
+    color: white;
+    font-size: 60px;
+    width: 550px;
+    font-family: Lato;
+    font-weight:bold;
+    letter-spacing: 1px;
+}
 .sign-in {
     background: #1a1d42;
     width: 100vw;
     height: 100%;
     display:grid;
-    place-content: center end;
+    grid-template-columns: 60% 40%;
+    place-content: center;
 }
 .comp {
     width: 600px;
